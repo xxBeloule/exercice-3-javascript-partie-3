4807ec132446dcd20607cf7d736dd6a2e84ed8f0
Beloule <trezaaqq@hotmail.fr>
2019-04-15 09:51:54 +0200

    Exercice3 partie 3 javascript

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var boutons=document.getElementsByClassName('color');
+
+boutons[0].addEventListener('click',function(){document.getElementById('text').style.color="green"});
+boutons[1].addEventListener('click',function(){document.getElementById('text').style.color="red"});
+boutons[2].addEventListener('click',function(){document.getElementById('text').style.color="blue"});
